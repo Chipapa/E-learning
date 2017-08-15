f3cf1c51fc8f63ffd6760cf697fc13e496145c89
Unknown <jmrq_30@yahoo.com>
2017-08-16 02:59:53 +0800

    Testing Session

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -2,6 +2,8 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+session_start();
+
 class Pages extends CI_Controller {
 
     public function __construct() {
@@ -48,12 +50,25 @@ class Pages extends CI_Controller {
             $result = $this->loginmodel->login($data);
 
             if ($result == TRUE) {
-                $this->load->view('pages/success');
-            } else {
-                $data = array(
-                    'error_message' => 'Invalid Username or Password.'
-                );
-                $this->load->view('pages/loginpage', $data);
+
+                $username = $this->input->post('username');
+
+                $result = $this->loginmodel->read_user_information($username);
+                if ($result != false) {
+                    $session_data = array(
+                        'username' => $result[0]->username,
+                        'usertype' => $result[0]->userType,
+                    );
+
+                    // Add user data in session
+                    $this->session->set_userdata('logged_in', $session_data);
+                    $this->load->view('pages/success');
+                } else {
+                    $data = array(
+                        'error_message' => 'Invalid Username or Password.'
+                    );
+                    $this->load->view('pages/loginpage', $data);
+                }
             }
         }
     }
