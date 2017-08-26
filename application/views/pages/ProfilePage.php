ee4699bd5e06a514f34ede1eb6218b7d4f8856f2
Unknown <johnjoseph0516@yahoo.com>
2017-08-26 16:57:01 +0800

    Add avatar in profile page

--- application/views/pages/ProfilePage.php
+++ application/views/pages/ProfilePage.php
@@ -9,6 +9,16 @@
 
 ?>
 <div class="container" id="mainDiv">
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="<?php echo base_url(); ?>/assets/png/avatar.png"">
+            <p align="" Avatong </p>
+        </div>
+        <div class="col-sm-8">
+            
+        </div>
     <?php echo  date('Y-m-d H:i:s'); ?>
     <b><a href="<?php echo site_url('pages/logout'); ?>">Logout</a></b>
+    </div>
 </div>
+
