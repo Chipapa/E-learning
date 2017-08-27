95e735401273bc05128de09aa444f22c791319b2
Unknown <johnjoseph0516@yahoo.com>
2017-08-27 21:43:38 +0800

    Merge Fix
    Finalize merge

--- application/controllers/Questions.php
+++ application/controllers/Questions.php
@@ -1,32 +1,19 @@
 <?php
 
-<<<<<<< HEAD
 Class Questions extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
         $this->load->model('questionsmodel');
         $this->load->helper('url_helper');
+        
+        $this->load->helper('form');
+        $this->load->library('form_validation');
     }
     public function index() {
         $data['questions'] = $this->questionsmodel->get_questions();
                     
         $this->view('LandingPage', $data);
-=======
-class Questions extends CI_Controller 
-{
- 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('QuestionsModel');
-        $this->load->helper('url_helper');
-        $this->load->library('session');
-        $this->load->helper('form');
-       $this->load->library('form_validation');
-        
-        
->>>>>>> Addquestion
     }
     
     public function view($page = false, $passData = false) {
@@ -39,18 +26,38 @@ class Questions extends CI_Controller
             $this->load->view('pages/headerLogin');
             $this->load->view('pages/' . $page, $passData);
             $this->load->view('pages/footer');
-<<<<<<< HEAD
         } else {
-=======
-        }else{
->>>>>>> Addquestion
             $this->load->view('pages/headerMain');
             $this->load->view('pages/' . $page, $passData);
             $this->load->view('pages/footer');
         }
     }
-<<<<<<< HEAD
-
+	
+	public function viewAskQuestion(){
+            $this->view('AskQuestionPage');
+	}
+	
+	public function create()
+    {
+        $data['title'] = 'Add Question  ';
+ 
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('question', 'Question', 'required');
+ 
+        if ($this->form_validation->run() === FALSE)
+        {
+            $this->view("askquestionpage");    
+        }
+        else
+        {
+            $this->questionsmodel->ask_question();
+//           $this->load->view('pages/headerMain', $dataTitle);
+//            $this->load->view('pages/' . $page, $passData);
+//            $this->load->view('pages/footer');
+                redirect("questions/index");
+            //echo 'Jesther gwpao';
+        }
+    }
     
     
     public function viewquestion($slug = NULL){
@@ -66,33 +73,3 @@ class Questions extends CI_Controller
         $this->view('answer_question_page', $data);
     }
 }
-=======
- 
-    public function index()
-    {   $this->view('AskQuestionPage');
-        //$this->load->helper('form');
-    }
-   public function create()
-    {
-        $data['title'] = 'Add Question  ';
- 
-        $this->form_validation->set_rules('title', 'Title', 'required');
-        $this->form_validation->set_rules('question', 'Question', 'required');
- 
-        if ($this->form_validation->run() === FALSE)
-        {
-            $this->view("askquestionpage");    
-        }
-        else
-        {
-            $this->QuestionsModel->ask_question();
-//           $this->load->view('pages/headerMain', $dataTitle);
-//            $this->load->view('pages/' . $page, $passData);
-//            $this->load->view('pages/footer');
-                $this->view('success');
-            //echo 'Jesther gwpao';
-        }
-    }
-
-}
->>>>>>> Addquestion
